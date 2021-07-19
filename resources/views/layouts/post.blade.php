82fbb3546a16667865bce93808a7e2847dfcbf1f
faraday123 <samueladams990@gmail.com>
2021-07-19 10:20:35 +0000

    first commit

---
+++ resources/views/layouts/post.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.app')
+
+
+
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <post-component></post-component>
+    </div>
+</div>
+
+@endsection
+
